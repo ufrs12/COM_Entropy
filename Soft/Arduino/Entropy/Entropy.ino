c4e4841a074aa58b5e5fbd0a9cb579e3b33d9067
ufrs12 <ufrs12@yandex.ru>
2025-10-01 03:39:32 +0300

    1

--- Soft/Arduino/Entropy/Entropy.ino
+++ Soft/Arduino/Entropy/Entropy.ino
@@ -2,6 +2,8 @@
 
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
+
+#include <7semi_DS18B20.h>
  
 #define MODBUS_SERIAL Serial
 #define MODBUS_BAUD 19200
@@ -15,23 +17,30 @@ uint16_t holdingRegisters[numHoldingRegisters];
 
 Adafruit_ADS1115 ads;
 
+DS18B20_7semi sensor(11);  // data pin
+
 void setup() {
   modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);
   MODBUS_SERIAL.begin(MODBUS_BAUD, MODBUS_CONFIG);
   modbus.begin(MODBUS_UNIT_ID, MODBUS_BAUD, MODBUS_CONFIG);
 
   ads.begin();  // Инициализация модуля ADS1115
+
+  uint8_t count = sensor.searchDevices();
 }
 
 void loop() {
   int16_t adc0;
   adc0 = ads.readADC_SingleEnded(0);  // Чтение значения с аналогового входа 0
-  
+  holdingRegisters[0] = adc0;
+
+  uint8_t addr[8];
+  if (sensor.getAddress(0, addr)) {
+    float t = sensor.readTemperature(addr);
+    holdingRegisters[2] = t * 100;
+  }
+
   modbus.poll();
 
   analogWrite(3, holdingRegisters[1]);      // отправляем на мосфет
-
-  holdingRegisters[0] = adc0;
-
-  
 }
